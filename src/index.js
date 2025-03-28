339f0e3ef2947500c990f08ce956c21b1b74d564
Brandon <binhnvhcm@gmail.com>
2025-03-28 21:36:13 +1100

    before using routes

--- src/index.js
+++ src/index.js
@@ -5,28 +5,43 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+// const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', engine({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs')
-// console.log('PATH: ', path.join(__dirname, 'resources/views'))
 app.set('views', path.join(__dirname, 'resources/views'))
 
-// console.log('PATH: ', path.join(__dirname, 'resources/views'))
-// console.log('PATH: ', __dirname)
+// route init
+// route(app)
 
 app.get('/', (req, res) => {
   res.render('home')
-})
+});
 
 app.get('/news', (req, res) => {
+  console.log('Query: ', req.query.q);
   res.render('news')
+});
+
+app.get('/search', (req, res) => {
+  res.render('search')
+});
+
+app.post('/search', (req, res) => {
+  console.log('Body: ', req.body);
+  res.send("");
 })
 
 app.listen(port, () => {
